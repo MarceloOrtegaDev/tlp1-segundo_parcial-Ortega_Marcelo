d3815acd2f4a5e9dfbf7fa5197ee711541c79d06
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2024-06-27 15:53:58 -0300

    Se completaron las rutas y controladores de los libros, en este caso se realizaron las rutas para postear un nuevo libro, borrar y editar

--- src/app.js
+++ src/app.js
@@ -22,6 +22,44 @@ app.get("/books/:id", (req, res)=>{
     }
 })
 
+app.post("/books", (req, res)=>{
+    const nuevosLibro = {
+        id: bdBooks.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+        year: parseInt(req.body.year)
+    }
+    if(bdBooks.some((titulo)=> titulo.title === nuevosLibro.title)) {
+        res.json({mensaje: "Ese libro ya existe"})
+    } else {
+        bdBooks.push(nuevosLibro);
+        res.json({mensaje: "Se subió el nuevo libro"})
+    }
+})
+
+app.put("/books/:id", (req, res)=>{
+    const id = parseInt(req.params.id);
+    const editarLibro = bdBooks.find((editarLibro)=> editarLibro.id === id);
+    if(editarLibro) {
+        editarLibro.title = req.body.title;
+        editarLibro.author = req.body.author;
+        editarLibro.year = req.body.year;
+        res.json({mensaje: "Se editó el libro"})
+    } else {
+        res.json({mensaje: "Libro no encontrado"})
+    }
+})
+
+app.delete("/books/:id", (req, res)=>{
+    const id = parseInt(req.params.id);
+    const bookdId = bdBooks.findIndex((bookId)=> bookId.id === id);
+    if(bookdId === -1) {
+        res.json({mensaje:"Ese id de libro no existe"})
+    } else {
+            bdBooks.splice(bookdId, 1);
+            res.json({mensaje: "Se eliminó el libro elegido"})
+        }
+})
 
 
 
